e142bd38bc8be3ed3e91e432bf58d15bcb1757c6
codeauslander <cesar.gusto.catano@gmail.com>
2018-07-31 23:38:33 -0500

    Added options for the uri request

--- adHoc/frontEnd/fetch/api/managed-records.js
+++ adHoc/frontEnd/fetch/api/managed-records.js
@@ -6,5 +6,29 @@ window.path = "http://localhost:3000/records";
 
 // Your retrieve function plus any additional functions go here ...
 
+function retrieve(options) {
+  var uri = new URI(window.path); 
+  if (options !== undefined) {
+    uri.setSearch({limit: 10});
+    switch (options.page) {
+      case undefined:
+          uri.setSearch({offset: 0});
+          break;
+      case 1:
+          uri.setSearch({offset: 0});
+          break;
+      case 2:
+          uri.setSearch({offset: 10});
+          break;
+      case 3:
+          uri.setSearch({offset: 20});
+          break;
+    }
+    if (options.colors !== undefined) {
+      uri.setSearch({'color[]': options.colors});
+    }
+  }
+  // here start the request using fetch fetch(path).then.....
+}
 
 export default retrieve;
