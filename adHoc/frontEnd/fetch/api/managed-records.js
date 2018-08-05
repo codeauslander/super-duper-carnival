4183ddabd7b8b0593df46084b45b4c1e3dc41da2
codeauslander <cesar.gusto.catano@gmail.com>
2018-08-04 23:58:01 -0500

    console.log data

--- adHoc/frontEnd/fetch/api/managed-records.js
+++ adHoc/frontEnd/fetch/api/managed-records.js
@@ -7,41 +7,73 @@ window.path = "http://localhost:3000/records";
 // Your retrieve function plus any additional functions go here ...
 
 function retrieve(options) {
+  console.log('options',options);
   var uri = new URI(window.path); 
   if (options !== undefined) {
     uri.setSearch({limit: 10});
-    switch (options.page) {
-      case undefined:
-          uri.setSearch({offset: 0});
-          break;
-      case 1:
-          uri.setSearch({offset: 0});
-          break;
-      case 2:
-          uri.setSearch({offset: 10});
-          break;
-      case 3:
-          uri.setSearch({offset: 20});
-          break;
+    if (options.page === undefined || options.page === 1) {
+      uri.setSearch({offset: 0});
+    } else {
+      uri.setSearch({offset: (options.page - 1) * 10});
     }
     if (options.colors !== undefined) {
       uri.setSearch({'color[]': options.colors});
     }
   }
   // here start the request using fetch fetch(path).then.....
-  fetch(uri).then(function (response) {
-    var contentType = response.headers.get("content-type");
-    return contentType;
-  }).then(function (contentType) {
-    if (contentType && contentType.indexOf('application/json' !== -1)) {
-      return contentType.json().then(function(data) {
-        var result = {};
-        if (options.page === undefined || options.page === 1) {
-          result.previousPage = null;
-        } 
-      })
-
+  return fetch(uri)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    var results = {};
+    switch (options.page) {
+      case undefined:
+          results.previousPage = null;
+          break;
+      case 1:
+          results.previousPage = null;
+          break;
+      default:
+          results.previousPage = (options.page - 1);
     }
+    
+    results.nexPage = null;
+    results.ids = [];
+    results.open = [];
+    results.closedPrimaryCount = 0;
+
+    data.forEach(function(object) {
+      results.ids.push(object.id)
+      if (object.disposition === 'open') {
+          results.open.push({
+              id: object.id,
+              color: object.color,
+              disposition: object.disposition,
+              isPrimary: (object.color === 'red' || object.color === 'blue' || object.color === 'yellow'
+                  ? true
+                  : false)
+          });
+      } else if (object.disposition === 'closed') {
+          results.closedPrimaryCount++;  
+      }
+    });
+
+    if (options.page === 1 && options.colors === undefined) {
+      results.nextPage = 2;
+    } else if (results.ids.length <= 10 || options.page > 2) {
+        results.nextPage = null;
+    } else if (options.page === undefined) {
+        results.nextPage = 2;
+    } else {
+        results.nextPage = (options.page + 1);
+    } 
+    console.log(results);
+    return results;
+  })
+  .catch(function(error) {
+    console.log(error,'error');
   });
 }
 
