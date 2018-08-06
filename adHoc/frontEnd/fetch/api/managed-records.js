59866682a43e474898e17cb1ad6d5a0da5ff2b8f
codeauslander <cesar.gusto.catano@gmail.com>
2018-08-06 00:19:57 -0500

    missing recursive fetch call

--- adHoc/frontEnd/fetch/api/managed-records.js
+++ adHoc/frontEnd/fetch/api/managed-records.js
@@ -1,80 +1,77 @@
 import fetch from "../util/fetch-fill";
 import URI from "urijs";
 
-// /records endpoint
 window.path = "http://localhost:3000/records";
 
-// Your retrieve function plus any additional functions go here ...
+function isPrimary(argument) {
+  return (argument === 'red' || argument === 'blue' || argument === 'yellow');
+}
+
+// function getSliceData(uri) {
+//   return fetch(uri)
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (allData) {
+//     function deleteData(allData) {
+//       if (allData.length > 10 ) {
+//       allData.shift();
+//       return deleteData(allData);
+//     }
+//     return allData;
+//     }
+//   });
+// }
 
 function retrieve(options) {
-  console.log('options',options);
+  console.log(options,options);
   var uri = new URI(window.path); 
+  var uriQuery = {'limit': 10, 'offset': 0, 'color[]': ["red", "brown", "blue", "yellow", "green"]};
   if (options !== undefined) {
-    uri.setSearch({limit: 10});
-    if (options.page === undefined || options.page === 1) {
-      uri.setSearch({offset: 0});
-    } else {
-      uri.setSearch({offset: (options.page - 1) * 10});
-    }
-    if (options.colors !== undefined) {
-      uri.setSearch({'color[]': options.colors});
-    }
+    uriQuery.limit = options.limit === undefined ? uriQuery.limit : options.limit;
+    uriQuery.offset = options.page === undefined ? uriQuery.offset : (options.page - 1);
+    uriQuery['color[]'] = options.colors === undefined ? uriQuery['color[]'] : options.colors;
   }
-  // here start the request using fetch fetch(path).then.....
+  uriQuery.limit = ( uriQuery.limit * (uriQuery.offset + 1) );
+  uri.setSearch(uriQuery);
+  console.log('uri',uriQuery)
   return fetch(uri)
   .then(function (response) {
     return response.json();
   })
+  .then(function (allData) {
+    // function deleteData(allData) {
+    //   if (allData.length > 10 ) {
+    //     return deleteData(allData.shift());
+    //   }
+    //   return allData;
+    // }
+    return allData;
+  })
+  // getSliceData(uri)
   .then(function (data) {
-    console.log(data);
-    var results = {};
-    switch (options.page) {
-      case undefined:
-          results.previousPage = null;
-          break;
-      case 1:
-          results.previousPage = null;
-          break;
-      default:
-          results.previousPage = (options.page - 1);
+    var results = {'ids': [], 'open': [], 'closedPrimaryCount': 0, 'previousPage': null, 'nextPage': 2};
+    if (uriQuery.offset !== 0) {
+      results.previousPage = (uriQuery.offset - 1);
+      results.nextPage = uriQuery.offset + 1;
     }
-    
-    results.nexPage = null;
-    results.ids = [];
-    results.open = [];
-    results.closedPrimaryCount = 0;
-
     data.forEach(function(object) {
-      results.ids.push(object.id)
-      if (object.disposition === 'open') {
-          results.open.push({
-              id: object.id,
-              color: object.color,
-              disposition: object.disposition,
-              isPrimary: (object.color === 'red' || object.color === 'blue' || object.color === 'yellow'
-                  ? true
-                  : false)
-          });
-      } else if (object.disposition === 'closed') {
-          results.closedPrimaryCount++;  
-      }
+        results.ids.push(object.id);
+        if (object.disposition === 'open') {
+            results.open.push({
+                id: object.id,
+                color: object.color,
+                disposition: object.disposition,
+                isPrimary: isPrimary(object.color),
+            });
+        } else if (object.disposition === 'closed' && isPrimary(object.color)) {
+            results.closedPrimaryCount = results.closedPrimaryCount + 1;  
+        }
     });
-
-    if (options.page === 1 && options.colors === undefined) {
-      results.nextPage = 2;
-    } else if (results.ids.length <= 10 || options.page > 2) {
-        results.nextPage = null;
-    } else if (options.page === undefined) {
-        results.nextPage = 2;
-    } else {
-        results.nextPage = (options.page + 1);
-    } 
-    console.log(results);
     return results;
   })
   .catch(function(error) {
     console.log(error,'error');
   });
 }
-
 export default retrieve;
